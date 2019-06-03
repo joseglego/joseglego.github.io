c433ed001a20109de4952a4975d697c8857754e6
Jose Lezama <joseglego@gmail.com>
2019-06-02 22:19:29 -0400

    feat(knowledge): include knowledge v0

---
+++ components/knowledge.jsx
@@ -0,0 +1,15 @@
+import './knowledge.scss'
+function Knowledge() {
+  return (
+    <div className='knowledge'>
+      <h2>Knowledge</h2>
+      <p>During last months, in my daily tasks I use React and JS (next gen, thanks Babel!).</p>
+      <p>I usually do Front end development with React, SCSS & Javascript. But, usually my side projects include Ruby on Rails, Django and, even, Ionic.</p>
+
+      <p>I use to work with other popular frameworks (Vue 1year, Angular 6months, AngularJS 3years+) always with Ruby on Rails or Django as Server side framework.</p>
+      <p>I'm a Software Engineer from Simón Bolívar University (USB, Venezuela), and I've being working as Web Developer since 2014.</p>
+    </div>
+  )
+}
+
+export default Knowledge
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,14 @@ import { Fragment } from 'react'
 import './index.scss'
 
 import Landing from '../components/landing'
+import Knowledge from '../components/knowledge'
 import Experience from '../components/experience'
 
 function Index() {
   return (
     <Fragment>
       <Landing />
+      <Knowledge />
       <Experience />
     </Fragment>
   )
