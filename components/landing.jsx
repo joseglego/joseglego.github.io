ac485d27b1ba60915a57bf87e29eb8cd5c4244d6
Jose Lezama <joseglego@gmail.com>
2019-06-05 06:56:44 -0400

    feat(landing): small changes, second cool version

--- components/landing.jsx
+++ components/landing.jsx
@@ -9,13 +9,13 @@ function Landing() {
         <span className='landing-name__primary'>Lezama González</span>
       </h2>
       <h2 className='landing__position'>
-        <span className='landing-position__primary'>FrontEnd</span>
+        <span className='landing-position__primary'>frontend</span>
         <span className='landing-position__break'> / </span>
-        <span>Fullstack</span>
+        <span className='landing-position__secondary'>fullstack</span>
       </h2>
-      <p>
+      <p className='landing__descriptions'>
         <span className='landing__description'>I am a passionate developer,  </span>
-        <span className='landing__description'> a lego fan, </span>
+        <span className='landing__description'> a lego fan </span>
         <span className='landing__description'> and a coffee lover. </span>
       </p>
     </div>
