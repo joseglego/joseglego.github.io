e666c28e2e8268e8873009cf3038fd532146d8d7
Jose Lezama <joseglego@gmail.com>
2019-06-13 05:49:18 -0400

    feat(experience): define cooler version of experience, v1

--- components/experience.jsx
+++ components/experience.jsx
@@ -3,19 +3,26 @@ import './experience.scss'
 
 function Experience() {
   const experiences = [
-    { url: 'https://www.beetrack.com/', name: 'Beetrack', position: 'Front-End Developer', start: 'Oct 2018', end: 'Present'},
-    { url: 'https://www.platan.us/', name: 'Platanus', position: 'Business Developer', start: 'Sep 2017', end: 'Oct 2018'},
-    { url: 'http://cleteci.com/', name: 'Cleteci', position: 'Web Developer; 1; 2', start: 'Sep 2014', end: 'Aug 2017'}
+    { url: 'https://www.beetrack.com/', image: '/static/images/beetrack-logo.png', name: 'Beetrack', position: 'Front-End Developer', start: 'Oct 2018', end: 'Present', description: "I've been working in core libs which will be used through the different services of the application. Also, working in small open source libs, new small features, new modules and even remaking old modules. Most of the work is focus on maps and interactions."},
+    { url: 'https://www.platan.us/', image: '/static/images/platanus-logo.png', name: 'Platanus', position: 'Business Developer', start: 'Sep 2017', end: 'Oct 2018', description: "Worked in up to 8 different projects, of different natures: open source, internal projects and external clients. Changed Web Mobile / Frontend / Backend / FullStack. I also learnt a bunch of new technologies/concepts like CI/CD, quality code and standards, etc."},
+    { url: 'http://cleteci.com/', image: '/static/images/cleteci-logo.png', name: 'Cleteci', position: 'Web Developer', start: 'Sep 2014', end: 'Aug 2017', description: "Also, I served as a Scrum Master & Technical Lead. Mentoring developers, reviewing code, etc. Including  new tools to improve the quality of the environment, code and product. Supporting integrations. Developing new features on RESTful APIS / Web / Web Mobile."}
   ]
 
   return (
     <div className='section experiences'>
-      <div className='section-uppertitle'>02.</div>
-      <h2 className='section-title'>Experience</h2>
+      <div className='section-uppertitle text-center'>02.</div>
+      <h2 className='section-title text-center'>Experience</h2>
       <ul> {
         experiences.map(experience => (
-          <li key={`experience-${experience.name}`}>
-            <a href={experience.url} target='_blank'>{experience.name}</a> ({experience.start} - {experience.end}) - {experience.position}
+          <li className='card experiences__experience' key={`experience-${experience.name}`}>
+            <a className='text-center' href={experience.url} target='_blank'>
+              <img className='experience__logo' src={experience.image} alt={experience.name} />
+              <span className='experience__name '>{experience.name}</span>
+            </a>
+            <span className='experience__period'>{experience.start} - {experience.end}</span>
+            <span className='experience__position'>{experience.position}</span>
+
+            <span className='experience__description'>{experience.description}</span>
           </li>
         ))
       }</ul>
