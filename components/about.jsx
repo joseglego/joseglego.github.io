0546aca21885a23095ba62bfad4e8213e43ab374
Jose Lezama <joseglego@gmail.com>
2019-06-28 18:20:58 -0400

    toStash about

--- components/about.jsx
+++ components/about.jsx
@@ -1,4 +1,5 @@
 import Section from './section'
+import './about.scss'
 
 function About () {
   return (
@@ -7,12 +8,9 @@ function About () {
       title='About'
       extraClasses='section--sec'
     >
-      <p>I'm a passionate developer and coffee lover, from Vargas, Venezuela. Living in Santiago, Chile.</p><br/>
-      <p>For the past few months, in my daily tasks, I've been using Javascript and React.</p>
-      <p>I usually do Front-End development with React, Redux, SCSS, Javascript and a little of Jest. But, usually my side projects include Ruby on Rails, Django and, even, Ionic.</p>
-
-      <p>I used to work with other popular frameworks (Vue 1year, Angular 6months, AngularJS 3years+) always with Ruby on Rails or Django as Server side framework.</p>
-      <p>I'm a Software Engineer from Simón Bolívar University (USB, Venezuela), and I've been working as a Web Developer since 2014.</p>
+      <p className='about__paragraph'>I'm a passionate developer and coffee lover, from Vargas, Venezuela. Living in Santiago, Chile.</p>
+      <p className='about__paragraph'>For the past few months, in my daily tasks, I've been using Javascript and React. I usually do Front-End development with React, Redux, SCSS, Javascript and a little of Jest. I used to work with other popular frameworks (Vue 1year, Angular 6months, AngularJS 3years+) always with Ruby on Rails or Django as Server side framework. But, usually my side projects include Ruby on Rails, Django and, even, Ionic.</p>
+      <p className='about__paragraph'>I'm a Software Engineer from Simón Bolívar University (USB, Venezuela), and I've been working as a Web Developer since 2014.</p>
     </Section>
   )
 }
