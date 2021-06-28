06a122a59029b892681c358ca6868dc7ee653a7c
José Lezama <jose.lezama@clevertech.biz>
2021-06-27 22:48:37 -0400

    core(): apply prettier and linters

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,5 +11,5 @@ export default function Home() {
       </Head>
       <h1>Joseg LEGO</h1>
     </div>
-  )
+  );
 }
