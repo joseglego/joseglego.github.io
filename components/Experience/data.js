19df141635f9f0202cde29fb5778fa7e1cda8e89
José Lezama <jose.lezama@clevertech.biz>
2021-08-11 08:36:23 -0400

    feat(Experience): update data with latests Jobs

--- components/Experience/data.js
+++ components/Experience/data.js
@@ -1,17 +1,39 @@
 export const experiences = [
   {
+    url: 'https://www.clevertech.biz/',
+    image: '/images/jobs/clevertech-logo.png',
+    name: 'Clevertech',
+    position: 'JavaScript Engineer',
+    start: 'Oct 2020',
+    end: 'Present',
+    description:
+      'Helping startups building internal and external products, pivoting ideas, and relaunching products. Always focused on client needs but with special care on quality. Discussing the application architecture, and new functionalities.'
+  },
+
+  {
+    url: 'https://www.globant.com/',
+    image: '/images/jobs/globant-logo.png',
+    name: 'Globant',
+    position: 'Web UI Developer',
+    start: 'Sept 2019',
+    end: 'Sept 2020',
+    description:
+      'I worked on different projects in LATAM Airlines, specifically in Flight Tickets and Awards (Flight Tickets Redeem) flow. As part of Globant, I was a mentor for other developers and worked in multiple extra activities.'
+  },
+
+  {
     url: 'https://www.beetrack.com/',
-    image: '/static/images/beetrack-logo.png',
+    image: '/images/jobs/beetrack-logo.png',
     name: 'Beetrack',
     position: 'Front-End Developer',
     start: 'Oct 2018',
-    end: 'Present',
+    end: 'Sept 2019',
     description:
-      "I've been working in core libs which will be used through the different services of the application. Also, working in small open source libs, new small features, new modules and even remaking old modules. Most of the work is focus on maps and interactions."
+      "I've been working in core libs which will be used through the different services of the application. Also, working in small open source libs, new small features, new modules and even remaking old modules. "
   },
   {
     url: 'https://www.platan.us/',
-    image: '/static/images/platanus-logo.png',
+    image: '/images/jobs/platanus-logo.png',
     name: 'Platanus',
     position: 'Business Developer',
     start: 'Sep 2017',
@@ -21,12 +43,12 @@ export const experiences = [
   },
   {
     url: 'http://cleteci.com/',
-    image: '/static/images/cleteci-logo.png',
+    image: '/images/jobs/cleteci-logo.png',
     name: 'Cleteci',
     position: 'Web Developer',
     start: 'Sep 2014',
     end: 'Aug 2017',
     description:
-      'Also, I served as a Scrum Master & Technical Lead. Mentoring developers, reviewing code, etc. Including  new tools to improve the quality of the environment, code and product. Supporting integrations. Developing new features on RESTful APIS / Web / Web Mobile.'
+      'Also, I served as a Scrum Master & Technical Lead. Mentoring developers, reviewing code, etc. Including  new tools to improve the quality of the environment, code and product. Developing features on RESTful APIS / Web / Web Mobile.'
   }
 ];
