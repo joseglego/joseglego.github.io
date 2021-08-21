84138dbe91e9b63d0ba6b293f076a02a473df2ea
José Lezama <jose.lezama@clevertech.biz>
2021-08-21 06:24:22 -0400

    feat(NavBar): have transparent BG by default change on scroll

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -6,9 +6,22 @@ import styles from './Navbar.module.css';
 
 function Navbar () {
   const { darkMode } = useTheme();
+  const [scrollPosition, setScrollPosition] = React.useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-      <div className={styles.navbar}>
+      <div className={`${styles.navbar} ${scrollPosition > 75 ? styles.activeNavbar : ''}`}>
         <div className={styles.navbarContainer}>
           <div className={`${styles.navbarBrand} ${darkMode ? styles.darkMode : ''}`}>JosegLEGO</div>
           <ul className={styles.navbarList}>
