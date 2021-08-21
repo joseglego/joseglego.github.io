010dd5bb811b9ef9af502d669960921654525b59
José Lezama <jose.lezama@clevertech.biz>
2021-08-21 07:33:17 -0400

    feat(Navbar): receive ActiveSectionIndex and show it

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,7 +4,7 @@ import { useTheme } from '../../providers/ThemeProvider';
 
 import styles from './Navbar.module.css';
 
-function Navbar () {
+function Navbar ({ activeSectionIndex }) {
   const { darkMode } = useTheme();
   const [scrollPosition, setScrollPosition] = React.useState(0);
   const handleScroll = () => {
@@ -20,16 +20,22 @@ function Navbar () {
     };
   }, []);
 
+  const sections = [
+    { id: '#home', title: 'Home' },
+    { id: '#about', title: 'About' },
+    { id: '#experience', title: 'Experience' },
+    { id: '#projects', title: 'Projects' },
+    { id: '#contact', title: 'Contact' }
+  ];
+
   return (
       <div className={`${styles.navbar} ${scrollPosition > 75 ? styles.activeNavbar : ''}`}>
         <div className={styles.navbarContainer}>
           <div className={`${styles.navbarBrand} ${darkMode ? styles.darkMode : ''}`}>JosegLEGO</div>
           <ul className={styles.navbarList}>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            {sections.map(({ id, title }, index) => (
+              <li key={id}><a href={id} className={index === activeSectionIndex ? styles.activeSection : ''}>{title}</a></li>
+            ))}
           </ul>
         </div>
       </div>
