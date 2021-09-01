f573a952bd38cd002e8dad20d6f1ba624841b2ef
José Lezama <jose.lezama@clevertech.biz>
2021-09-01 05:42:32 -0400

    core(Config): define all social links in one file for reuse

---
+++ config.js
@@ -0,0 +1,12 @@
+const data = {
+  analyticsId: process.env.NEXT_PUBLIC_ANALYTICS_ID,
+  oldJoseglego: 'https://old.joseglego.io',
+  socialAll: 'josegLEGO',
+  socialEmail: 'me@joseglego.io',
+  socialGithubUser: 'joseglego',
+  socialGithubUrl: 'https://github.com/joseglego',
+  socialLinkedin: 'in/joseglego/',
+  socialLinkedinUrl: 'https://www.linkedin.com/in/joseglego/'
+};
+
+export default data;
