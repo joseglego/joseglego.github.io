bfa1f3573006ef00d0671909a4b3cf24766d26b0
José Lezama <jose.lezama@clevertech.biz>
2021-09-01 05:51:26 -0400

    feat(Footer): define footer V2

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,3 +1,7 @@
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+import data from '../../config';
+
 import styles from './Footer.module.css';
 
 function Footer () {
@@ -7,7 +11,15 @@ function Footer () {
         <div className={styles.footerInfo}>
           <div className={styles.joseglego}>JosegLEGO</div>
           <p>Made from scratch by josegLEGO. Using <a href="https://nextjs.org/">Next.js</a>, and hosted in <a href="https://www.netlify.com/">Netlify</a>.</p>
-          <p>You can find the code in <a href="https://github.com/joseglego/joseglego.github.io/tree/feat/next-version">its Github Repo</a>, feel free to use it or provide feedback.</p>
+          <p>You can find the code in <a href="https://github.com/joseglego/joseglego.github.io/tree/feat/next-version">its Github Repo</a>, feel free to use it or provide feedback. Also, you can see the <a target="_blank" href={data.oldJoseglego} rel="noopener noreferrer">2016 version</a></p>
+        </div>
+        <div className={styles.footerSocial}>
+          <a className={styles.footerSocialLink} target="_blank" href={data.socialLinkedinUrl} rel="noopener noreferrer">
+            <FaLinkedinIn aria-label="LinkedIn" />
+          </a>
+          <a className={styles.footerSocialLink} target="_blank" href={data.socialGithubUrl} rel="noopener noreferrer">
+            <FaGithub aria-label="GitHub" />
+          </a>
         </div>
       </div>
     </div>
